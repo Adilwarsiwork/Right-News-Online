025e81f9066e0f6b6c6726ad8251339365e2bd57
Adil Warsi <132612437+Adilwarsiwork@users.noreply.github.com>
2023-07-29 13:04:44 +0530

    Update nav-itemTwo.js

--- components/layout/navbar/nav-itemTwo.js
+++ components/layout/navbar/nav-itemTwo.js
@@ -17,9 +17,9 @@ export default function NavItemTwo({
 
   return (
     <li className={`nav-item ${menuItems ? "nav-item-has-children" : ""}`}>
-      <a href="#" className="nav-link-item drop-trigger" onClick={handleClick}>
+      <span className="nav-link-item drop-trigger" onClick={handleClick}>
         {navItemText}
-      </a>
+      </span>
 
       {!megaMenu ? (
         <DropdownMenu isOpenDropDown={isOpenDropDown} menuItems={menuItems} />
